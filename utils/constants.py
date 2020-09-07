0136f1d84bba0eca59ea85a96af1d5e583e75cd8
Sergii Rubanenko <sergii.rubanenko@gmail.com>
2020-09-07 09:37:59 +0300

    Transformed to Enum classes

--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,13 @@
-parsing_expression = r'(\d{13})(::!(.+)!::/.*)((\w{8}-\w{4}-\w{4}-\w{4}-\w{12,}.)(dirty$|clean$))'
-data_base_name = "workbase.db"
-work_log_file = "work.log"
+from enum import Enum
+
+
+class FrameConstants(Enum):
+    """Stores the value for common project definitions"""
+    parsing_expression = r'(\d{13})(::!(.+)!::/.*)((\w{8}-\w{4}-\w{4}-\w{4}-\w{12,}.)(dirty$|clean$))'
+
+
+class NameConstants(Enum):
+    """Stores the names for project entities"""
+    data_base_name = "work_base.db"
+    work_table_name = "work_table"
+    work_log_file = "work.log"
